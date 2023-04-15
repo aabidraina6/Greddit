6ed3614b946b543e7ebdac72a0c16810c864ed01
aabidraina6 <aabidraina5@gmail.com>
2023-04-15 09:18:22 +0530

    all removed

--- frontend/src/components/protectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { Outlet , Navigate } from "react-router-dom"
-
-
-export default function ProtectedRoute(){
-    const isAuth = localStorage.getItem("isLoggedIn") == "true"
-    return isAuth ? <Outlet/> : <Navigate to="/login"/>;
-}
-
